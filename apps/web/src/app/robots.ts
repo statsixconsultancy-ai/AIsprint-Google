1656a30a46ed34b3a784543e3d04586cf98d938b
vibbeit-cyber <vibbeit@gmail.com>
2026-04-07 03:34:20 +0530

    Site Map

--- apps/web/src/app/robots.ts
+++ apps/web/src/app/robots.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next'
 
 export default function robots(): MetadataRoute.Robots {
-  const baseUrl = 'https://www.aisprint.in'
+  const baseUrl = 'https://www.goaisprint.com'
 
   return {
     rules: [
